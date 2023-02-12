7d3c24f405e61876d28f9a1382bdf6cc25e25fe6
hooman mousavi <hoomanmousavi77@gmail.com>
2023-02-13 00:42:40 +0330

    posts process converted to cookie base auth

--- client/src/composables/usePosts.ts
+++ client/src/composables/usePosts.ts
@@ -1,15 +1,20 @@
 import {IPosts} from "~/composables/useTypes";
 
 export const usePosts= ()=>{
-    const {endpoints}=useAppConfig()
+    const {public:{endpoints,cookieName}}=useRuntimeConfig()
     const posts=useState<IPosts[]|null>('posts',()=>[])
     const fetchFlag=useState<boolean>('postFlag',()=>false)
     onMounted(async ()=>{
         try {
-            const headers:any=useRequestHeaders()
-            const data=await $fetch<IPosts[]>(endpoints.posts,{headers})
+            const xsrf=useCookie(cookieName)
+            const data=await $fetch<IPosts[]>(endpoints.posts,{
+                credentials:'include',
+                headers:{
+                    'Accept':'application/json',
+                    'X-XSRF-TOKEN':xsrf.value as string
+                }
+            })
             posts.value=data
-            console.log(posts.value)
             fetchFlag.value=true
         }catch (err) {
             fetchFlag.value=false
