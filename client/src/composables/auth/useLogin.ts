ff6c034ca61f2a79f38bcaccb4d2e8e4115dddb7
hooman mousavi <hoomanmousavi77@gmail.com>
2023-02-13 00:29:18 +0330

    login process converted to cookie base auth

--- client/src/composables/auth/useLogin.ts
+++ client/src/composables/auth/useLogin.ts
@@ -1,7 +1,7 @@
 import {Login_Information,Login_Response} from "~/composables/useTypes";
 import { useToast } from "vue-toastification";
 export const useLogin=()=>{
-    const {endpoints}=useAppConfig()
+    const {public:{endpoints,cookieName}}=useRuntimeConfig()
     const toast = useToast();
     const userData=useState<Login_Response['user']|null>('userInfo',()=>null)
     const errors=ref<string[]>([])
@@ -14,16 +14,25 @@ export const useLogin=()=>{
     const loginHandler = async () => {
         fetchFlag.value=true
         try {
-            const data:Login_Response['user']=await $fetch(endpoints.login,{
+            await $fetch(endpoints.csrf,{
+                credentials:'include'
+            })
+            const xsrf=useCookie(cookieName)
+            const data:Login_Response=await $fetch(endpoints.login,{
                 method:'POST',
-                body:userInformation
+                body:userInformation,
+                credentials:'include',
+                headers:{
+                    'Accept':'application/json',
+                    'X-XSRF-TOKEN':xsrf.value as string
+                }
             })
             toast.success('You are logged in!')
             errors.value=[]
-            userData.value=data
+            userData.value=data.user
             return navigateTo({name:'index'})
         }catch (err:any) {
-            errors.value=Object.values(err.data.data).flat() as string[]
+            errors.value=Object.values(err.data).flat() as string[]
         }finally {
             fetchFlag.value=false
         }
