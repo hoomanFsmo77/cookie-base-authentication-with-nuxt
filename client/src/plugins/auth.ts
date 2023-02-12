98af14ade027eede4de3679d3e70f1e96a171093
hooman mousavi <hoomanmousavi77@gmail.com>
2023-02-13 00:34:57 +0330

    auto login process converted to cookie base auth

--- client/src/plugins/auth.ts
+++ client/src/plugins/auth.ts
@@ -1,15 +1,22 @@
 import {Login_Response} from "~/composables/useTypes";
 
 export default defineNuxtPlugin(async ()=>{
-    // const headers:any=useRequestHeaders(['cookie'])
-    // const userData=useState<Login_Response['user']|null>('userInfo',()=>null)
-    // const {endpoints}=useAppConfig()
-    // try {
-    //     const data:Login_Response=await $fetch(endpoints.me,{headers})
-    //     userData.value=data.user
-    // }catch (err){
-    //     userData.value=null
-    // }
+    const {public:{endpoints}}=useRuntimeConfig()
+    const headers:any=useRequestHeaders(['cookie'])
+    const userData=useState<Login_Response['user']|null>('userInfo',()=>null)
+    try {
+        const data:Login_Response=await $fetch(endpoints.me,{
+            credentials:'include',
+            headers:{
+                ...headers,
+                'Accept':'application/json',
+                'Referer':'http://localhost:3000',
+            }
+        })
+        userData.value=data.user
+    }catch (err){
+        userData.value=null
+    }
 
 
 })
